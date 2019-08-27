b821b89dc7c9499154d556a487919c9de5778530
Marian Caron <caron.marian@gmail.com>
2019-08-27 17:59:27 +0200

    therapist policy created

---
+++ test/controllers/therapists_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TherapistsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get therapists_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get therapists_index_url
+    assert_response :success
+  end
+
+end
