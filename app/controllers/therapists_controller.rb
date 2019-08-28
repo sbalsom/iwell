a4db9710392507bc5816c5e1be898f7921e5e9f3
Sadie Balsom <saralogsin@protonmail.com>
2019-08-28 10:25:01 +0200

    added an elastic search for therapist name and language

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,9 +1,19 @@
 class TherapistsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   skip_after_action :verify_authorized, only: :show
+
   def index
+    if params[:query].present?
+      sql_query = "first_name ILIKE :query \
+      OR last_name ILIKE :query \
+      OR language ILIKE :query"
+      @therapists = policy_scope(Therapist).where(sql_query, query: "%#{params[:query]}%")
+    else
     @therapists = policy_scope(Therapist)
   end
+  end
+
+
 
   def show
     @therapist = policy_scope(Therapist).find(params[:id])
