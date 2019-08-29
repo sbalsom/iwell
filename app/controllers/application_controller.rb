5769aa67e54f0155a5484bab93316d74dc10f586
Marian Caron <caron.marian@gmail.com>
2019-08-29 18:35:07 +0200

    added strong param for the user :name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # [...]
    before_action :authenticate_user!
@@ -16,6 +17,13 @@ class ApplicationController < ActionController::Base
    #   flash[:alert] = "You are not authorized to perform this action."
    #   redirect_to(root_path)
    # end
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
    private
 
