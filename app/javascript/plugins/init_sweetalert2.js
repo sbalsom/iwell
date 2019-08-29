2d08355ba8ced6e2106e598e53a052c87b620630
Sadie Balsom <saralogsin@protonmail.com>
2019-08-29 16:57:22 +0200

    temporarily disabled javascript for development of backend

--- app/javascript/plugins/init_sweetalert2.js
+++ app/javascript/plugins/init_sweetalert2.js
@@ -1,13 +1,13 @@
-import swal from 'sweetalert2';
+// import swal from 'sweetalert2';
 
-const initSweetalert2 = (selector, options = {}, callback = () => {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', (event) => {
-      swal.fire(options).then(callback); // <-- add the `.then(callback)`
-      event.preventDefault()
-    });
-  }
-};
+// const initSweetalert2 = (selector, options = {}, callback = () => {}) => {
+//   const swalButton = document.querySelector(selector);
+//   if (swalButton) { // protect other pages
+//     swalButton.addEventListener('click', (event) => {
+//       swal.fire(options).then(callback); // <-- add the `.then(callback)`
+//       event.preventDefault()
+//     });
+//   }
+// };
 
-export { initSweetalert2 };
+// export { initSweetalert2 };
