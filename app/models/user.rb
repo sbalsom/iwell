a99aae6e29290f33d46d9a61f2b6cbc1847ffd01
Marian Caron <caron.marian@gmail.com>
2019-08-29 18:32:03 +0200

    added users validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
+  validates :name, presence: true
 end
