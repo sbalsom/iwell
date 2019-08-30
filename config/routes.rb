c286c0c3c3feee9dbccb11b9c5554a0db3727e59
Marian Caron <caron.marian@gmail.com>
2019-08-30 12:22:29 +0200

    added bookings#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard', to: "dashboard#show"
   resources :therapists, only: [:index, :show] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:show, :new, :create]
     resources :reviews, only: [:new, :create]
   end
   devise_for :users
