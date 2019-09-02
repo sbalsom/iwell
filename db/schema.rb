cdbc8ff96fc794b72f34bbf0f717c75e6dd059d8
Marian Caron <caron.marian@gmail.com>
2019-09-02 12:47:35 +0200

    removed user name migration as it was already implemented and wasn't possible to start local server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_143105) do
+ActiveRecord::Schema.define(version: 2019_08_29_185047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_143105) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.bigint "therapist_id"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["therapist_id"], name: "index_users_on_therapist_id"
