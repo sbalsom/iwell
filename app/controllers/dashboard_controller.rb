ce55613bffe8994e5843e3ed4e6b63c269fb73c4
Sadie Balsom <saralogsin@protonmail.com>
2019-09-04 10:11:22 +0200

    now the completed sessions will show in past booking

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,11 +11,11 @@ class DashboardController < ApplicationController
    else
     @user_therapist = nil
     end
-    @today_booking = Booking.where(starts_at: Date.today).last
+    @today_booking = Booking.where(["starts_at = ? and status <> ?", Date.today, 3]).last
     # AND not status:completed
     @bookings = current_user.bookings
     @next_booking = @bookings.where("starts_at > ?", Date.today)
-    @past_bookings = @bookings.where("starts_at < ?", Date.today)
+    @past_bookings = @bookings.where(["starts_at < ? or status = ? ", Date.today, 3])
     # OR status: completed
 
     if current_user.therapist_id
