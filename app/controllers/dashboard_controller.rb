416a33cf020d9d76c80cbbf2905359ace03a36aa
bartu culha <culhabartu@gmail.com>
2019-09-04 14:31:24 +0200

    switching to master

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,17 +4,22 @@ class DashboardController < ApplicationController
   def show
     @user = current_user
     if @user.therapist_id
-      @user_therapist = Therapist.find(@user.therapist_id)
-    else
-      @user_therapist = nil
+     @user_therapist = @user.therapist
+   else
+    @user_therapist = nil
     end
-    @today_booking = Booking.where("starts_at > ? AND starts_at < ?", Time.now.beginning_of_day, Time.now.end_of_day)
-    # @todays_booking = Booking.where(starts_at: Date.today)
-    @next_bookings = current_user.bookings.where("starts_at > ?", Date.today)
-    @past_bookings = current_user.bookings.where("starts_at < ?", Date.today)
-    # @booking_therapists = bookings.map do |booking|
-    #   Therapist.find(booking.therapist_id)
-    # end
+    @today_booking = Booking.where(
+      "starts_at > ? AND starts_at < ?",
+      Time.now.beginning_of_day,
+      Time.now.end_of_day
+    ).first
+    @bookings = current_user.bookings
+    @next_booking = @bookings.where("starts_at > ?", Time.now.end_of_day)
+    @past_bookings = @bookings.where("starts_at < ?", Time.now.beginning_of_day)
+    #bookings.sort_by do |booking| booking.starts_at
+
+   # @booking_therapists = bookings.map do |booking|
+    #  Therapist.find(booking.therapist_id)
     if current_user.therapist_id
       @my_therapist = Therapist.find(current_user.therapist_id)
     end
